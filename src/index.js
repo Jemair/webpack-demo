703da64ecdf77c7b83c600933f9fc5e0fbd41197
王廉铭 <liam.wang@linyuefengdeMac-mini.local>
2018-04-03 23:23:52 +0800

    stylus-loader

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import s from './index.styl'
+
+console.log(s)
+
 document.write('Hello webpack')
 console.log('Hello webpack')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: 'babel-loader',
+      include: [resolve('src'), resolve('test')],
+    }, {
+      test: /\.styl$/,
+      use: [{
+        loader: 'file-loader',
+      }, {
+        loader: 'extract-loader',
+      }, {
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+        }
+      }, {
+        loader: 'stylus-loader'
+      },]
+    }]
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'assets/index.html'
     })
-  ]
+  ],
 }
